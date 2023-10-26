766c84aecbef95d433ec983964d45d2a617993b9
Bipin <bipinpal737@gmail.com>
2023-10-26 19:41:12 +0530

    Check Prime number

---
+++ prime.js
@@ -0,0 +1,27 @@
+function isprime(num)
+{
+    let count=0;
+    for(let i=1; i<num; i++)
+    {
+        if(num%i==0)
+        {
+            count++
+        }
+    }
+    if(count==2)
+    {
+        return false
+    }
+    else
+    {
+        return true
+    }
+}
+
+    for(let i=0; i<=20; i++)
+    {
+        if (isprime(i))
+        {
+            console.log(i);
+        }
+    }
